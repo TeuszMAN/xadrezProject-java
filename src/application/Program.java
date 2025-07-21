ae5619f0836a4885d52e18a720c9213d96b43973
Mateus Alves <gamertalverxd@gmail.com>
2025-07-21 07:01:11 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
 
     public static void main (String[] args) {
-        System.out.println("Oii");
+        Board board = new Board(8,8);
     }
 }
